819696424641ad16e02b50d3e4313b8e11ba0725
Jamison Pereira <jamison.pereira@gmail.com>
2022-11-17 17:31:46 +0000

    Sequelize essentials

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
-const layout = require('./views/layout')
+const layout = require('./views/layout');
+const { db, Page, User } = require('./models');
+
 app.use(morgan('dev'));
-app.use(express.static(__dirname+'/public'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.send(layout(''));
-})
+  res.send(layout(''));
+});
+
+db.authenticate().then(() => {
+  console.log('connected to the database');
+});
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`App listening in port ${PORT}`)
-})
+const init = async () => {
+  await db.sync();
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+};
+
+init();
